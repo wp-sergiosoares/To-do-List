614c099053777f8ce47caaf75731c95be211fd8d
Sérgio Soares <sergiosoares@Mac-mini-de-Sergio.local>
2025-06-18 01:12:02 +0100

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://wp-sergiosoares.github.io/Lista-de-Compras-Interativa/",
   plugins: [react(), tailwindcss()],
 });
