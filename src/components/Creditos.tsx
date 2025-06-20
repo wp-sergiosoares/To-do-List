99a42b01e5516ba5b2dda204e8ee9ebf19651403
Sérgio Soares <sergiosoares@Mac-mini-de-Sergio.local>
2025-06-20 11:35:28 +0100

    fix clrs creditos

--- src/components/Creditos.tsx
+++ src/components/Creditos.tsx
@@ -2,12 +2,12 @@ import { Mail, MessageCircleMore } from "lucide-react";
 
 export default function Creditos() {
   return (
-    <div className="mt-15 text-center text-gray-300 text-sm space-y-1">
+    <div className="mt-15 text-center text-gray-300 dark:text-gray-600 text-sm space-y-1">
       <div className="">
         <a
           href="https://portfolio-two-peach-25.vercel.app/"
           target="_blank"
-          className=" hover:text-gray-400"
+          className=" hover:text-gray-400 transition"
         >
           Desenvolvido por Sérgio Soares
         </a>
@@ -16,7 +16,7 @@ export default function Creditos() {
         <a
           href="mailto:virtual.sergiosoares@gmail.com"
           target="_blank"
-          className=" hover:text-gray-400 flex gap-2 items-center justify-center"
+          className=" hover:text-gray-400 flex gap-2 items-center justify-center transition"
         >
           <Mail size={18} />
           virtual.sergiosoares@gmail.com
@@ -26,7 +26,7 @@ export default function Creditos() {
         <a
           href="https://wa.me/351914410560"
           target="_blank"
-          className=" hover:text-gray-400 flex gap-2 items-center justify-center"
+          className=" hover:text-gray-400 flex gap-2 items-center justify-center transition"
         >
           <MessageCircleMore size={18} />
           91 4410560
