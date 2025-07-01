c5b096453a48586c63dee234e739acaafa65e887
Sérgio Soares <sergiosoares@Mac-mini-de-Sergio.local>
2025-07-01 18:54:25 +0100

    add shadcn

--- src/components/Tickets/Item/ItemBtnDelete.jsx
+++ src/components/Tickets/Item/ItemBtnDelete.jsx
@@ -1,14 +1,41 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Trash2 } from "lucide-react";
 
 export default function ItemBtnDelete({ onRemove, item }) {
   return (
-    <div className="flex items-center justify-center">
-      <button
-        onClick={() => onRemove(item.id)}
-        className="text-[var(--color-secondary)] cursor-pointer p-1"
-      >
-        <Trash2 size={18} />
-      </button>
-    </div>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <div className="flex items-center justify-center">
+          <button className="text-[var(--color-secondary)] cursor-pointer p-1">
+            <Trash2 size={18} />
+          </button>
+        </div>
+      </AlertDialogTrigger>
+      <AlertDialogContent className="bg-white">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Tem a certeza?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Esta ação não pode ser desfeita. Esta ação irá remover
+            permanentemente a tarefa da sua lista.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction onClick={() => onRemove(item.id)}>
+            Continuar
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
