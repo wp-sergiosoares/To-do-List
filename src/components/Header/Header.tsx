e9dbac1ecf5f19f9cd9238fe451ebd2570777c3c
Sérgio Soares <sergiosoares@Mac-mini-de-Sergio.local>
2025-07-01 18:45:04 +0100

    feat(header): animate logo with fade-in effect

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,32 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import SplitText from "gsap/SplitText";
+import { useRef } from "react";
+
+gsap.registerPlugin(SplitText);
+
 export default function Header() {
+  const container = useRef(null);
+
+  useGSAP(
+    () => {
+      let mySplitText = new SplitText(".logo", {
+        type: "chars",
+      });
+
+      gsap.from(mySplitText.chars, {
+        duration: 0.3,
+        y: 20,
+        autoAlpha: 0,
+        stagger: 0.02,
+        ease: "back.out",
+      });
+    },
+    { scope: container }
+  );
   return (
-    <div className="text-center">
-      <h1 className="text-4xl text-[var(--color-primary)] font-bold tracking-tighter font-[Nunito]">
+    <div ref={container} className="text-center">
+      <h1 className="logo text-4xl font-bold tracking-tighter font-[Nunito] text-[var(--color-primary)]">
         Lista de Tarefas
       </h1>
     </div>
